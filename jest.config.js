d1464d5dccbfe7aa9644142616833adfeadb0cf7
rakhee28 <rakhee@strings.ai>
2025-02-28 15:25:28 +0530

    Jest unit test case configuration

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.svg$': 'jest-transformer-svg'
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      useESM: true,
+    }]
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx']
+};
