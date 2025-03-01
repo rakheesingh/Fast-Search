c59d25d0abfa293cec764aeabd793a77b1632348
rakhee28 <rakhee@strings.ai>
2025-03-01 13:28:40 +0530

    added screen for error message

---
+++ src/common/errorScreen/ErrorWithMessage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface ErrorComponentProps {
+  message: string;
+}
+
+const ErrorComponent: React.FC<ErrorComponentProps> = ({ message }) => {
+  return (
+    <div className="flex items-center justify-center min-h-screen" data-testId= "error-state" >
+      <div className="bg-red-100 border border-red-400 text-red-700 rounded shadow p-4 w-full max-w-lg h-[200px] flex items-center justify-center">
+        <span className="text-center text-xl font-medium">{message}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorComponent;
